e645e2491d8b8632d9f5c9b250bb524e473ad6f5
Elena Antonenko <e.antonenko@whirl.sg>
2020-07-16 10:47:17 +0300

    add gitignore

--- specs/distributionOfDicePoints.spec.js
+++ specs/distributionOfDicePoints.spec.js
@@ -4,6 +4,6 @@ describe('Distribution for dice', function () {
 
     it('Distribution', async function () {
         await distribution.checkDistribution(); // distribution for 1 dice
-        // await distribution.checkDistribution(2, 12, 11, 500); // distribution for 2 dices (total roll points is a sum of 2 dices)
+        await distribution.checkDistribution(2, 12, 11, 500); // distribution for 2 dices (total roll points is a sum of 2 dices)
     });
 });
